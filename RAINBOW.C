b6d97e9ab3ab6b70d484bf618e8c3ea755892f42
Pradip <98959751+Shiyal-Pradip@users.noreply.github.com>
2022-03-16 16:38:58 +0530

    Add files via upload
    Make a Rainbow on OutPut Window

---
+++ RAINBOW.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+void main()
+{
+	int Gd=0,Gm,R;
+	initgraph(&Gd,&Gm,"C:\\TURBOC3\\BGI");
+	for(R=20; R<328; R++)
+		{
+			delay(50);
+			setcolor(R/10);
+			arc(320,370,0,180,R);
+		}
+// getch();
+ closegraph();
+}
