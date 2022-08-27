8ede155e2650802efd68c1ab93c8c3fb766f7731
Pradip <98959751+Shiyal-Pradip@users.noreply.github.com>
2022-08-27 06:16:26 +0530

    Create Pattern1.c
    YouTube Video:-

---
+++ Pattern In C Graphics/Pattern1.c
@@ -0,0 +1,22 @@
+#include<conio.h>
+#include<graphics.h>
+void main()
+{
+	int Gd=0,Gm,i,R;
+	initgraph(&Gd,&Gm,"C:\\TC\\BGI");
+
+	setfillstyle(WIDE_DOT_FILL,YELLOW);
+	circle(325,240,23);
+	floodfill(326,246,WHITE);
+
+	for(i=0; i<125; i++)
+		{
+			arc(325-i,240,90+i,180,10+i);
+			arc(325+i,240,0+i,90,10+i);
+			arc(325-i,240,180+i,-90,10+i);
+			arc(325+i,240,-90+i,0,10+i);
+			setcolor(i/10);
+			delay(40);
+		}
+ getch();
+}
